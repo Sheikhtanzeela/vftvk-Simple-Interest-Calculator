92d0bf3156df4b8cef12f64cacc80a3a33300eb0
Sheikh Tanzeela <Sheikhtanzeela590@gmail.com>
2021-05-20 11:16:33 +0530

    Assignment

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 function compute() {
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var year = new Date().getFullYear() + parseInt(years);
+  var principal = document.getElementById("principal").value;
+  const amt = Number(principal);
+  console.log(amt);
+  var interest = (principal * years * rate) / 100;
+  var amount = interest;
   p = document.getElementById("principal").value;
+  if (p < 0) {
+    alert("Enter a positive number");
+  }
+  document.getElementById("principal").focus();
+
   document.getElementById("result").innerHTML =
-    "If you deposit " +
+    "<p> If you deposit <span style = background-color:#F0B929;>" +
     principal +
-    ",<br>at an interest rate of " +
+    "</span> ,<br>at an interest rate of <span style = background-color:#F0B929;>" +
     rate +
-    "%<br>You will receive an amount of " +
+    "%</span><br>You will receive an amount of <span style = background-color:#F0B929;>" +
     amount +
-    ",<br>in the year " +
+    "</span>,<br>in the year " +
     year +
-    "<br>";
+    "<br></p>";
+  return interest;
 }
-
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = (principal * years * rate) / 100;
-var year = new Date().getFullYear() + parseInt(years);
-
 function updateRate() {
-  var reteval = document.getElementById("rate").value;
-  document.getElementById("rate_val").innerHTML = rateval;
+  var inter = compute();
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+  document.getElementById("result").innerText = inter;
 }
